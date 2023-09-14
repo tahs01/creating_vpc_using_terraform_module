0865525624d6661763fc6cd45789197fdc2ba957
Than Htike Soe <131392064+tahs01@users.noreply.github.com>
2023-09-15 00:25:59 +0630

    Updated by tahs

--- versions.tf
+++ versions.tf
@@ -10,7 +10,6 @@ terraform {
 provider "aws" {
   shared_config_files      = ["/home/vagrant/.aws/config"]
   shared_credentials_files = ["/home/vagrant/.aws/credentials"]
-  profile                  = "<name_of_profile>"
-  # alias                  = "thanhtikesoe"              
+  profile                  = "<name_of_profile>"             
   region                   = var.aws_region
 }
